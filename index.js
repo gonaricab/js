68597a1402be9c1850a8940b4c0f15a65eb01132
gonzalo <gonaricab@gmail.com>
2022-10-27 20:24:17 -0300

    ok

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 function funcion (variable1,variable2) {
     let x = variable1 * variable2  
-    alert (x)
+    alert (x)    
 }
 
 let producto = parseInt(prompt ("1. Iphone - 2. TV - 3. Ipad - 4. PC"));
 let seguirComprando = true
 let decision
+
 while (seguirComprando === true) {
     if (producto === 1) {
         let cantidad = parseInt(prompt("Indica cantidad:")) 
         funcion (500, cantidad)
+       
     }
     else if (producto === 2) {
         let cantidad = parseInt(prompt("Indica cantidad:"))
         funcion (300,cantidad)
+        
     }
     else if (producto === 3) {
         let cantidad = parseInt(prompt("Indica cantidad:"))
         funcion (800,cantidad)
+        
     }
     else if (producto === 4) {
         let cantidad = parseInt(prompt("Indica cantidad:"))
         funcion (1000,cantidad)
+        
     }
     else {
         producto = parseInt(prompt ("Producto habilitado: 1. Iphone - 2. TV - 3. Ipad - 4. PC"))
@@ -34,4 +39,5 @@ while (seguirComprando === true) {
         seguirComprando = false
     }
 }
+
 
