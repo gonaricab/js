f50eb8995dc637b60f0f69e15b586da06053f1a8
gonzalo <gonaricab@gmail.com>
2022-11-12 18:29:53 -0300

    agregue array, class, objetos, metodo find

--- index.js
+++ index.js
@@ -3,47 +3,53 @@ function funcion (variable1,variable2) {
     alert ("Valor de la compra " + x)
 }
 
-let producto = parseInt(prompt ("1. Iphone - 2. TV - 3. Ipad - 4. PC"));
+let productoSeleccionado = parseInt(prompt ("1. Iphone - 2. TV - 3. Ipad - 4. PC"));
 let seguirComprando = true
 let decision
+const productosExistentes = []
 
-while (seguirComprando === true) {
-    if (producto === 1) {
-        let cantidad = parseInt(prompt("Indica cantidad:")) 
-        funcion (500, cantidad)
-
-       
-    }
-    else if (producto === 2) {
-        let cantidad = parseInt(prompt("Indica cantidad:"))
-        funcion (300,cantidad)
-      
-        
-    }
-    else if (producto === 3) {
-        let cantidad = parseInt(prompt("Indica cantidad:"))
-        funcion (800,cantidad)
-       
-        
-    }
-    else if (producto === 4) {
-        let cantidad = parseInt(prompt("Indica cantidad:"))
-        funcion (1000,cantidad)
-       
-        
-    }
-    else {
-        producto = parseInt(prompt ("Producto habilitado: 1. Iphone - 2. TV - 3. Ipad - 4. PC"))
-    }
-    decision = parseInt (prompt ("Deseas seguir comprando? 1-Sí 2-No"))
-    if (decision === 1){
-        producto = parseInt(prompt ("1. Iphone - 2. TV - 3. Ipad - 4. PC"));
-    }
-    else {
-        seguirComprando = false
-       
+class Producto {
+    constructor (id, producto, precio) {
+        this.id = id
+        this.producto = producto
+        this.precio = precio
     }
 }
 
+const iphone = new Producto (1, "iphone", 500)
+productosExistentes.push (iphone)
+const tv = new Producto (2, "tv", 300)
+productosExistentes.push (tv)
+const ipad = new Producto (3, "ipad", 800)
+productosExistentes.push (ipad)
+const pc = new Producto (4, "pc", 1000)
+productosExistentes.push (pc)
 
 
+const productoCliente = productosExistentes.find (producto => producto.id === productoSeleccionado)
+
+
+while (seguirComprando === true) {
+    if(productoCliente) {
+        let cantidad = parseInt(prompt("Indica cantidad:")) 
+        funcion (productoCliente.precio, cantidad)
+    }
+    else {
+        productoSeleccionado = parseInt(prompt ("Productos habilitados: 1. Iphone - 2. TV - 3. Ipad - 4. PC"))
+
+    }
+    decision = parseInt (prompt ("Deseas seguir comprando? 1-Sí 2-No"))
+    if (decision === 1){
+        productoSeleccionado = parseInt(prompt ("1. Iphone - 2. TV - 3. Ipad - 4. PC"));
+    }
+    else {
+        seguirComprando = false
+    }
+
+}
+
+   
+
+
+ 
+  
