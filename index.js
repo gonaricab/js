f82e441974c0d58e70a9f45a2bff50fb80cece63
gonzalo <gonaricab@gmail.com>
2022-11-19 00:38:52 -0300

    agregue filter para saber si  hay productos mas baratos o no

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function funcion (variable1,variable2) {
-    let x = variable1 * variable2  
-    alert ("Valor de la compra " + x)
+    return  variable1 * variable2  
+    
 }
 
 let productoSeleccionado = parseInt(prompt ("1. Iphone - 2. TV - 3. Ipad - 4. PC"));
@@ -27,14 +27,27 @@ productosExistentes.push (pc)
 
 while (seguirComprando === true) {
     const productoCliente = productosExistentes.find (producto => producto.id === productoSeleccionado)
+    console.log (productoCliente)
     if(productoCliente) {
         let cantidad = parseInt(prompt("Indica cantidad:")) 
-        funcion (productoCliente.precio, cantidad)
-       
+        let x = funcion (productoCliente.precio, cantidad)
+        let i = productoCliente.producto
+        let y = productoCliente.precio 
+        let productoMasBarato = productosExistentes.filter (producto => producto.precio < productoCliente.precio)
+        let h = []
+        productoMasBarato.forEach (elemento => h.push (elemento.producto))
+        console.log (productoMasBarato)
+        console.log (h)
+        if (productoMasBarato.length > 0) {
+            alert (`Valor de la compra: ${x} dolares.\nCompraste: ${i}.\nExisten estos productos mas baratos: ${h.join(", ")}.`)
+        }
+        else {
+            alert (`Valor de la compra: ${x} dolares. \nCompraste ${i}.\nNo existen productos mas baratos.`)
+        }
     }
     else {
         productoSeleccionado = parseInt(prompt ("Productos habilitados: 1. Iphone - 2. TV - 3. Ipad - 4. PC"))
-
+        continue
     }
     decision = parseInt (prompt ("Deseas seguir comprando? 1-Sí 2-No"))
     if (decision === 1){
