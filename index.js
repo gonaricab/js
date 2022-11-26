806d9395df648c6408bdf3f10e8780e3bb65c286
gonzalo <gonaricab@gmail.com>
2022-11-26 15:40:21 -0300

    agregue css y otras cosas

--- index.js
+++ index.js
@@ -14,15 +14,19 @@ class Producto {
         this.precio = precio
     }
 }
-const iphone = new Producto (0, "iphone", 500)
+const iphone = new Producto (0, "Apple iPhone 14 Pro Max (1 TB) - Morado oscuro", 500)
 
-const tv = new Producto (1, "tv", 300)
+const tv = new Producto (1, "Smart TV Samsung Series 7 4K 50", 300)
 
-const ipad = new Producto (2, "ipad", 800)
+const ipad = new Producto (2, "Apple iPad Air de 10.9 WI-FI 64GB Gris espacia", 800)
 
-const pc = new Producto (3, "pc", 1000)
+const pc = new Producto (3, "Pc Armada Intel Core I5 8gb Ssd240 W10 Office", 1000)
 
-const productos = [iphone,tv,ipad,pc]
+const reloj = new Producto (4, "Apple Watch Series 3 (GPS) - Correa deportiva negro", 200)
+
+const monitor = new Producto (5, "Monitor Led Samsung 24'' Con Diseño Sin Bordes ", 210)
+
+const productos = [iphone,tv,ipad,pc, reloj, monitor]
 
 let precioTotal = 0
 
@@ -39,23 +43,31 @@ boton.onclick = () => {
     
 }
 
-divDos.innerHTML = `<select id="select"></select>
+divDos.innerHTML = `
+    <div class="productos">
+    <h7>Productos:</7>
+    <select id="select"></select>
     <button id="agregar">Agregar al Carrito</button>
     <button id="comprar">Comprar</button>
-    <button id="vaciar">Vaciar Carrito</button>`
+    <button id="vaciar">Vaciar Carrito</button>
+    <div>`
+    
 productos.forEach((producto)=>{
     const optionProd = document.createElement("option")
-    optionProd.innerText = `${producto.producto}${producto.precio}`
+    optionProd.innerText = `${producto.producto} - Precio: $ ${producto.precio}`
     select.append(optionProd)
 }
 )
+
 const botonAgregar = document.getElementById("agregar")
 const selectProds = document.getElementById("select")
 const botonComprar = document.getElementById("comprar")
 const vaciarCarrito = document.getElementById("vaciar")
 const ul = document.getElementById("ul")
+const carritoVacio = document.getElementById("carritoVacio")
 
 botonAgregar.onclick = () => {
+    carritoVacio.innerText = ("")
     const indexProducto = selectProds.selectedIndex
     const productoSeleccionado = productos[indexProducto]
     carrito.push(productoSeleccionado)
@@ -63,7 +75,7 @@ botonAgregar.onclick = () => {
     localStorage.setItem("carrito",JSON.stringify(carrito))
     x = JSON.parse(localStorage.getItem("carrito"))
     const listItem = document.createElement("li")
-    listItem.innerText = `${productoSeleccionado.producto}`
+    listItem.innerText = `${productoSeleccionado.producto} - Precio: $ ${productoSeleccionado.precio}`
     ul.append(listItem)
     
 }
@@ -79,7 +91,7 @@ vaciarCarrito.onclick = () => {
 botonComprar.onclick = () => {
   const carritoValor = JSON.parse(localStorage.getItem("carrito"))
   if (carritoValor === null) {
-    alert("no tenes nada en el carrito")
+    carritoVacio.innerText = ("El carrito está vacío")
   }
   else {
     carritoValor.forEach((element) => {
